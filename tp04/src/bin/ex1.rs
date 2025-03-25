bb37e35f1088d4cf6014bad47a921747d114e56d
endy090 <lamarielhadi1@gmail.com>
2025-03-25 12:27:47 +0100

    Update ex1.rs

--- tp04/src/bin/ex1.rs
+++ tp04/src/bin/ex1.rs
@@ -1,3 +1,32 @@
 use std::io;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn main() {}
+fn main() {
+    // Utilisation de Box<T> pour allouer un entier sur le tas
+    let number = Box::new(10);
+    println!("Valeur dans la Box : {}", number);
+
+    // Création d'un compteur partagé entre plusieurs threads
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for _ in 0..5 {
+        let counter_clone = Arc::clone(&counter);
+        
+        let handle = thread::spawn(move || {
+            let mut num = counter_clone.lock().unwrap();
+            *num += 1;
+            println!("Compteur mis à jour par un thread : {}", num);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Valeur finale du compteur : {}", *counter.lock().unwrap());
+}
