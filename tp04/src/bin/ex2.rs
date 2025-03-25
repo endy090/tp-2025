08340e6c009b68a1f7f584e3db2794eb8ebab652
endy090 <lamarielhadi1@gmail.com>
2025-03-25 12:29:55 +0100

    Update ex2.rs

--- tp04/src/bin/ex2.rs
+++ tp04/src/bin/ex2.rs
@@ -1,5 +1,10 @@
 use std::io;
-
 fn main() {
-    println!("Hello, world!");
+    let mut text = Box::new(String::from("Message initial: "));
+    println!("Entrez un texte : ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Erreur de lecture");
+    text.push_str(&input);
+    println!("Texte final : {}", text);
 }
+
