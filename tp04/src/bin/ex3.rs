d966091d79deeb6ff1cbc308ac62c2d2fbc84e15
endy090 <lamarielhadi1@gmail.com>
2025-03-25 12:34:58 +0100

    Update ex3.rs

--- tp04/src/bin/ex3.rs
+++ tp04/src/bin/ex3.rs
@@ -1,6 +1,23 @@
-use std::io;
-use std::os::unix::io::AsRawFd;
+use nix::unistd::{fork, ForkResult};
+use nix::sys::wait::waitpid;
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child }) => {
+            println!("Parent process: PID = {}", process::id());
+            println!("Child created with PID = {}", child);
+            
+            // Attendre que l'enfant se termine
+            waitpid(child, None).unwrap();
+            println!("Child process terminé.");
+        }
+        Ok(ForkResult::Child) => {
+            println!("Child process: Je suis le processus enfant !");
+            process::exit(0); // Terminer le processus enfant proprement
+        }
+        Err(_) => {
+            eprintln!("Échec du fork !");
+        }
+    }
 }
